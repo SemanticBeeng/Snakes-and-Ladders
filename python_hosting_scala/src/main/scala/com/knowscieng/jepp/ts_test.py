2b4c84982f49d954928bb3f39850ff65a76e54f3
SemanticBeeng <nick@semanticbeeng.com>
2018-03-11 14:15:46 -0700

    Can use `pandas` and `r2py` from `jep`

--- python_hosting_scala/src/main/scala/com/knowscieng/jepp/ts_test.py
+++ python_hosting_scala/src/main/scala/com/knowscieng/jepp/ts_test.py
@@ -2,11 +2,9 @@ import pandas as pd
 from datetime import datetime
 from rpy2.robjects import r, pandas2ri
 
-#from com.knowscieng.jepp.sclaython import *
-
 pandas2ri.activate()
 
-r_df = r['iris'].head()
+r_df = r['iris']
 
 p_df = pd.DataFrame(r_df)
 
@@ -15,16 +13,52 @@ print(p_df)
 
 ts = pd.Timestamp(datetime(2012, 5, 1))
 
-# # TsUtils.ts = jep.Jep.getValue(ts)
-# #
-# # TsUtils.df = p_df
-#
-# ageGroup = ScalaUtils.ageGroupMethod(21.0)
-# print(ageGroup + 1)
-#
-# ages = ScalaUtils.ages()
-# print(ages.head().toString())
-#
-# people = ScalaUtils.people()
-# print(people)
+from rpy2.robjects.packages import importr
+
+graphics = importr('graphics')
+grdevices = importr('grDevices')
+base = importr('base')
+stats = importr('stats')
+
+import array
+
+x = array.array('i', range(10))
+y = stats.rnorm(10)
+
+grdevices.X11()
+
+graphics.par(mfrow = array.array('i', [2,2]))
+graphics.plot(x, y, ylab = "foo/bar", col = "red")
+
+kwargs = {'ylab':"foo/bar", 'type':"b", 'col':"blue", 'log':"x"}
+graphics.plot(x, y, **kwargs)
+
+
+m = base.matrix(stats.rnorm(100), ncol=5)
+pca = stats.princomp(m)
+graphics.plot(pca, main="Eigen values")
+stats.biplot(pca, main="biplot")
+
+# ---
+from rpy2 import robjects as ro
+
+ro.r('''
+        # create a function `f`
+        f <- function(r, verbose=FALSE) {
+            if (verbose) {
+                cat("I am calling f().\n")
+            }
+            2 * pi * r
+        }
+        # call the function `f` with argument value 3
+        ''')
+
+r_f = ro.globalenv['f']
+
+print(r_f.r_repr())
+
+res = r_f(3)
+
+print(type(res))
+print(res)
 
